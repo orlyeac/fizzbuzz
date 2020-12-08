7a75bd96a6ff8cd511f75f6eeee6ccf80456c25c
Orlando Abreu <oabreu@uclv.cu>
2020-12-08 15:35:53 -0500

    including main

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,3 +5,7 @@ class FizzBuzz():
     def __init__(self):
         self.fizzbuzz = ['fizz'*(i % 3 == 0) + 'buzz'*(i % 5 == 0) or str(i) for i in range(1, 101)]
    
+
+if __name__ == "__main__":
+    print('\n'.join(FizzBuzz().fizzbuzz))
+
